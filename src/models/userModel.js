60771276fecf4102f7653e3a154cb59585acbd2f
jai84322 <jai84322@gmail.com>
2022-07-06 11:50:16 +0530

    initialized-project

---
+++ src/models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        enum: ["Mr", "Mrs", "Miss"],
+        trim: true
+    },
+    name: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    address: {
+        street: { type: String, },
+        city: { type: String },
+        pincode: { type: String }
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('user', userSchema)
